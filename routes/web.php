fc1fef4866207a73e364d4f43a5daec19692b92f
saad <saadsuri67@gmail.com>
2018-12-27 17:33:31 +0500

    Image Filter

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@
 */
 
 Route::get('/','CrawlController@index');
-Route::get('/image','ImageController@addAlphaColor');
+
+Route::get('/create-image','ImageController@addAlphaColor');
+Route::get('/image','ImageController@image');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
