c607ea1fc7c537b20cf6d5554a1f2cf20585ae37
Saad Suri <saadsuri67@gmail.com>
2019-04-11 08:09:25 +0500

    helo

--- resources/views/quotes/show.blade.php
+++ resources/views/quotes/show.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
 
-<section class="mt-2 mb-2">
+<section class="mt-4 mb-2">
 	
 	<div class="container">
 		<div class="row">
@@ -21,7 +21,7 @@
                                	<blockquote class="blockquote py-1">
     	                            <p class="lead">{{$quote->body}}</p>
     	                            <footer class="blockquote-footer">
-                                        {{-- <a href="{{$quote->author->link}}" ><cite class="font-weight-600" title="Source Title">{{$quote->author->name}}</cite></a> --}}
+                                        <a href="{{$quote->author->link}}" ><cite class="font-weight-600" title="Source Title">{{$quote->author->name}}</cite></a>
                                     </footer>
 		                        </blockquote>
                                 {{ $quote->tags }}
@@ -30,35 +30,49 @@
 
                             <div class="card-footer">
 
-                                <div class="d-flex justify-content-between">
-                                    <div>
+                                <div class="d-flex justify-content-between mn-mh">
+                                    
                                         @if(!is_null($navigation->first()))
                                             <a href="{{$navigation->first()->link}}" class="btn btn-primary btn-animated btn-animated-x">
                                                 <span class="btn-inner--visible">Previous</span>
                                                 <span class="btn-inner--hidden"><i class="far fa-arrow-left"></i></span>
                                             </a>
                                         @endif
+                                    
+                                     <div style="padding-top:10px">
+                                        <ul class="list-inline mb-0">
+                                            <li class="list-inline-item pr-4">
+                                                <a href="#" class="text-muted"><i class="far fa-bookmark mr-1 text-muted"></i> 131</a>
+                                            </li>
+                                            <li class="list-inline-item pr-4">
+                                                <a href="#" class="text-muted"><i class="far fa-eye mr-1 text-muted"></i> 255</a>
+                                            </li>
+                                        </ul>
                                     </div>
-                                    <div>
-                                        {{-- <a href="#"><h3>{{$quote->author->name}}</h3></a> --}}
-                                    </div>
-
-                                    <div>
                                         @if(!is_null($navigation->last()))
                                             <a href="{{$navigation->last()->link}}" class="btn btn-primary btn-animated btn-animated-x">
                                                 <span class="btn-inner--visible">Next</span>
                                                 <span class="btn-inner--hidden"><i class="far fa-arrow-right"></i></span>
                                             </a>
                                         @endif
-                                    </div>
+                                </div>
+
+                                <div class="row mt-5">
+                                   
                                 </div>
                                
                             </div>
-                        </div>
+                </div>
 			</div>
 
 			<div class="col-lg-4">
-				
+				<div class="media align-items-center">
+                    {{-- <span class="avatar avatar-lg bg-pink rounded mr-4">FD</span> --}}
+                    <img src="http://whatsmyquote.com/images/avatar-medium-avatar-e7ddc48127277056b38626526c7fd22918f18da0.jpg" class="avatar avatar-lg rounded">
+                    <div class="media-body">
+                        <a href="#" class="h5 mb-0">Front End Developer</a>
+                    </div>
+                </div>
 			</div>
 		</div>
 	</div>
